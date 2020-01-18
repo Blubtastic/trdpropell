90bbda96d1d2553a07b54d9407c16a0a318065c8
Blubtastic <martin.storodegard@gmail.com>
2020-01-18 15:02:20 +0100

    basic propeller hat structure

--- trdpropell/src/App.js
+++ trdpropell/src/App.js
@@ -1,24 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './hat.png';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Welcome to my webpage!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Do not learn react!
-        </a>
-      </header>
+    <div className="page">
+      <h1>Trondheim Propellhattklubb</h1>
+      
+      <div className="content">
+        <img src={logo} />
+      </div>
+
+      <div className="tabs">
+        <div className="tab"></div>
+        <div className="tab"></div>
+        <div className="tab"></div>
+      </div>
     </div>
   );
 }
